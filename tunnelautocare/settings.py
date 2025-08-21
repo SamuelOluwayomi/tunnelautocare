7a07a1a1dbe84d4de2ed5f3fa12d173b1c264ad0
Samuel <samuelfaith500@gmail.com>
2025-08-21 19:05:36 +0100

    Removing superuser block

--- tunnelautocare/settings.py
+++ tunnelautocare/settings.py
@@ -143,23 +143,3 @@ STATICFILES_DIRS = [BASE_DIR / 'static']  # Your project-level static folder
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
-if os.environ.get('CREATE_SUPERUSER') == '1':
-    import django
-    django.setup()
-    from django.core.management import call_command
-    from django.contrib.auth import get_user_model
-
-    # Run migrations
-    print("📦 Running migrate...")
-    call_command("migrate")
-
-    # Optional: Create superuser
-    User = get_user_model()
-    username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
-    email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
-    password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin123')
-
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username=username, email=email, password=password)
-        print("✅ Superuser created")
