ea80e6a42abe5fec6cca0b8e825f7a2044ec61aa
Samuel <samuelfaith500@gmail.com>
2025-08-24 18:34:30 +0100

    adding review to my admin.py

--- webpages/admin.py
+++ webpages/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Service
-from .models import ContactMessage
+from .models import Service, ContactMessage, Review
 
 @admin.register(Service)
 class ServiceAdmin(admin.ModelAdmin):
@@ -11,3 +10,9 @@ class ServiceAdmin(admin.ModelAdmin):
 class ContactMessageAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'created_at')
     search_fields = ('name', 'email')
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('fullname', 'rating', 'approved', 'created_at')
+    list_filter = ('approved', 'rating', 'created_at')
+    search_fields = ('fullname', 'message')
